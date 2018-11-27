4ea950e1fe706e404e5d1677f21c4e1e51f90f3d
Kuei Peng <ragnakuei@livemail.tw>
2018-11-28 07:28:28 +0800

    initial commit

---
+++ ClassLibrary/ClassBL.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ClassLibrary
+{
+    public class ClassBL
+    {
+        private readonly ILogger logger;
+
+        public ClassBL(ILogger<ClassBL> _logger)
+        {
+            this.logger = _logger;
+        }
+
+        public void Log()
+        {
+            logger.LogTrace("Loggin Level = 0 (Trace)");
+            logger.LogDebug("Loggin Level = 1 (Debug)");
+            logger.LogInformation("Loggin Level = 2 (Information)");
+            logger.LogWarning("Loggin Level = 3 (Warning )");
+            logger.LogError("Loggin Level = 4 (Error)");
+            logger.LogCritical("Loggin Level = 5 (Critical)");
+        }
+    }
+}
---
+++ NLogPractice/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using ClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLogPractice.Models;
+
+namespace NLogPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ClassBL _classBL;
+
+        public HomeController(ClassBL classBL)
+        {
+            this._classBL = classBL;
+        }
+        
+        public IActionResult Index()
+        {
+            _classBL.Log();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
